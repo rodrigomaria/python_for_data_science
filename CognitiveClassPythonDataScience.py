a47a5ed24abf0a4144364bd6a3b10abf3c16e521
Rodrigo Antônio Menezes Maria <rammaria@restinga.ifrs.edu.br>
2019-03-04 00:32:03 -0300

    update last day

--- CognitiveClassPythonDataScience.py
+++ CognitiveClassPythonDataScience.py
@@ -532,3 +532,38 @@ print(y)
 
 import matplotlib.pyplot as plt
 plt.plot(x,y)
+
+# numpy 2d Arrays
+a = [[11,12,13],[21,22,23],[31,32,33]]
+
+import numpy as np
+
+A = np.array(a)
+
+print(A.ndim)
+print(A.shape)
+print(A.size)
+print(A[1][2])
+
+#--slicing
+print(A[0:2,2])
+
+#--operations
+X = np.array([[1,0],[0,1]])
+Y = np.array([[2,1],[1,2]])
+Z = X + Y
+print(Z)
+
+Y = np.array([[2,1],[1,2]])
+Z = 2*Y
+print(Z)
+
+X = np.array([[1,0],[0,1]])
+Y = np.array([[2,1],[1,2]])
+Z = X*Y
+print(Z)
+
+A = np.array([[0,1,1],[1,0,1]])
+B = np.array([[1,1],[1,1],[-1,1]])
+C = np.dot(A,B)
+print(C)
